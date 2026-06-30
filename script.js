faeb7d0ee6b0385d47829f5b2e1d3d0687415b49
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-06-30 10:33:09 +0800

    Use the previous Book constructor from the other lesson practice

---
+++ script.js
@@ -0,0 +1,24 @@
+function Book (title, author, pages, read){
+
+    if(!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    
+    if(read === true){
+        this.read = "read";
+    }
+
+    if(read === false){
+        this.read = "not read yet";
+    }
+
+    this.info = function(){
+        return (`${this.title} by ${this.author}, ${this.pages}, ${this.read}`);
+    }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R.", 295, false);
+console.log(theHobbit.info());
